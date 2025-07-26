a6478be65c62e2e603cb9287ade117088fd01d93
divinlikele <divinlikele@gmail.com>
2025-07-26 21:47:39 +0100

    Add files via upload
    communauté du futur
    construisons ensemble un monde plus intelligent, plus solidaire, plus durable.
    plateforme éducative, technologique et communautaire dédiée aux jeunes d'haujourd'hui et aux bâtisseurs de demain.

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const scrollElements = document.querySelectorAll(".animate-on-scroll");
+
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add("is-visible");
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove("is-visible");
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+            } 
+            // Optional: To re-animate when scrolling up
+            // else {
+            //     hideScrollElement(el);
+            // }
+        });
+    };
+
+    // Initial check
+    handleScrollAnimation();
+    window.addEventListener("scroll", () => {
+        handleScrollAnimation();
+    });
+
+});
+
